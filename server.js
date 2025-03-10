e57eb1f383b731771b7928a0c2ef7dd5ac577199
ayush gupta <aerospace141@gmail.com>
2025-03-10 13:41:58 +0530

    Update server.js

--- server.js
+++ server.js
@@ -92,9 +92,9 @@ app.get("/",(req,res) => {
 })
 
 // Start the server
-// app.listen(PORT, () => {
-//   console.log(`Server is running on http://localhost:${PORT}`);
-// });
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
 
-module.exports = app;
-module.exports.handler = serverless(app);
+// module.exports = app;
+// module.exports.handler = serverless(app);
