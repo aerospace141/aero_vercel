918e7d33a1a9de464b7416097263b6d1cf099643
ayush gupta <aerospace141@gmail.com>
2025-03-10 13:23:46 +0530

    Update server.js

--- server.js
+++ server.js
@@ -85,6 +85,9 @@ app.use(express.json());
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
 
+app.get("/",(req,res) => {
+  res.status(200).send("hi, Its working.")
+})
 
 // Start the server
 app.listen(PORT, () => {
